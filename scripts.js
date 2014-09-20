b4f89c4a0c439168b2cc6615205c46bd7fa23451
Brendan Crawford <bcgir87@gmail.com>
2014-09-20 13:57:36 -0700

    added method to add/remove roommates from the list

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ var rent = 0; // monthly rent
 var sqft = 0; // total house square feet (complex calc)
 
 $(document).ready(function () {
+    // function wich causes the mode to show up
     var display_mode = function () {
         if (mode) {
             $("#mode").text("Mode: Complex");
@@ -12,14 +13,25 @@ $(document).ready(function () {
         }
     };
 
+    // button adds roommate to the array
     $("#add_rm_button").click(function () {
         var name = $("#name").val()
-        $("#roommates").append("<h3>" + name + "</h3>");
+        if (name != "") {
+        var roommate = $("<h3>" + name + "</h3>");
+        $("#roommates").append(roommate);
+        $(roommate).click(function () {
+            delete mates[name];
+            $(roommate).remove();
+        });    
         mates[name] = 0; 
+        }
+        for (n in mates) {
+            console.log(n);
+        }
     });
 
+    // when switch button clicked, changes modes
     $("#switch").click(function () {
-        console.log("switch");
         if (mode) {
             mode = false;
         } else {
@@ -27,5 +39,7 @@ $(document).ready(function () {
         }
         display_mode();
     });
+
+    // causes mode to show up on first load
     display_mode();
 });
